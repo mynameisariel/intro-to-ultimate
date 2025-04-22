38d8089b11ef0675b2c22e1096a3d80041860a4e
mynameisariel <cutiepanda2468@gmail.com>
2025-04-22 08:51:15 -0400

    new changes for swirl

---
+++ script.js
@@ -0,0 +1,12 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+ function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
